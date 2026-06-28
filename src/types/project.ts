ca6b919645d5a5e73251f9f349861dc8282f2d02
Steven <106054873+stevenpartida@users.noreply.github.com>
2026-06-27 23:18:47 -0700

    chore(type): add project coordinates to ProjectWithThumbnail

--- src/types/project.ts
+++ src/types/project.ts
@@ -2,6 +2,8 @@ import { Project } from "./db";
 
 export type ProjectWithThumbnail = Project & {
   thumbnail_url: string | null;
+  project_lat: number | null;
+  project_lng: number | null;
 };
 
 export type ProjectSettings = Pick<
