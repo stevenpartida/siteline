796953678907396962814fafe7e8220252b4d181
Steven <106054873+stevenpartida@users.noreply.github.com>
2026-07-29 23:41:51 -0700

    chore(pwa): replace app icons and add favicons

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -12,12 +12,12 @@ export default function manifest(): MetadataRoute.Manifest {
     orientation: "portrait",
     icons: [
       {
-        src: "/icons/icon-192x192.png",
+        src: "/icons/siteline-icon-192.png",
         sizes: "192x192",
         type: "image/png",
       },
       {
-        src: "/icons/icon-512x512.png",
+        src: "/icons/siteline-icon-512.png",
         sizes: "512x512",
         type: "image/png",
       },
