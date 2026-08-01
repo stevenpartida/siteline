e9ddde1549b998b22662f08e168c2a8a6ec703c6
Steven <106054873+stevenpartida@users.noreply.github.com>
2026-08-01 02:28:48 -0700

    refactor: change styling of input group and change validation rules for sign in and sign out schemas

--- src/lib/validators/auth.ts
+++ src/lib/validators/auth.ts
@@ -2,11 +2,12 @@ import { z } from "zod";
 
 // Sign Up Schema and Sign Up form values type
 export const signUpSchema = z.object({
-  full_name: z.string().min(2, "Name must be at least 2 characters."),
-  email: z.email("Enter a valid email."),
+  first_name: z.string().min(1, "Enter your first name"),
+  last_name: z.string().min(1, "Enter your last name"),
+  email: z.email("Enter a valid email address"),
   password: z
     .string()
-    .min(8, "Password must be at least 8 characters.")
+    .min(8, "Password doesn't meet all requirements")
     .regex(/[A-Z]/, "Must include 1 uppercase letter")
     .regex(/[0-9]/, "Must include 1 number")
     .regex(/[^\w\s]/, "Must include 1 symbol"),
@@ -16,8 +17,8 @@ export type SignUpFormValues = z.infer<typeof signUpSchema>;
 
 // Sign In schema and sign in form values type
 export const signInSchema = z.object({
-  email: z.email("Enter a valid email."),
-  password: z.string().min(1, "Password is required."),
+  email: z.email("Enter a valid email address"),
+  password: z.string().min(1, "Enter your password"),
 });
 
 export type SignInFormValues = z.infer<typeof signInSchema>;
